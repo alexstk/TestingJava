52cbc13228ca920d3b31a550023682701f5964ab
Alexander Ossa <aossa@intergrupo.com>
2019-04-26 10:44:19 -0500

    Test de integración con base de datos: búsqueda de 1 película y guardar una película. Parte 1.

--- javatests/src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
+++ javatests/src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
@@ -2,6 +2,8 @@ package com.platzi.javatests.movies.data;
 
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +12,7 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -17,15 +20,22 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MovieRepositoryIntegrationTest {
-    @Test
-    public void load_all_movies() throws SQLException {
-        DataSource dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
+
+    private MovieRepositoryJdbc movieRepository;
+    private DataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+        dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        MovieRepositoryJdbc movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+        movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+    }
 
+    @Test
+    public void load_all_movies() throws SQLException {
         Collection<Movie> movies = movieRepository.findAll();
 
         assertThat(movies, is(Arrays.asList(
@@ -33,5 +43,17 @@ public class MovieRepositoryIntegrationTest {
                 new  Movie(2, "Memento", 113, Genre.THRILLER),
                 new  Movie(3, "Matrix", 136, Genre.ACTION)
         )));
+    }
+
+    @Test
+    public void load_movie_by_id() {
+        Movie movie = movieRepository.findById(2);
+        assertThat(movie, is(new  Movie(2, "Memento", 113, Genre.THRILLER)));
+    }
+
+    @After  // Another way to avoid this is use the @BeforeClass instead of @Before an @After
+    public void tearDown() throws Exception {
+        Statement stmt = dataSource.getConnection().createStatement();
+        stmt.execute("shutdown"); // Both work correct: "shutdown"   "drop all objects delete files"
     }
 }
